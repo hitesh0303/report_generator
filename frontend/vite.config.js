fef3bce998b6e2a6afd8547ec7757d7fe2b9e524
Hitesh Bharat Sonawane <your.email@example.com>
2025-04-17 00:33:09 +0530

    resolved build errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa'],
+      output: {
+        globals: {
+          'react-icons/fa': 'ReactIconsFa'
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react-icons']
+  }
 })
